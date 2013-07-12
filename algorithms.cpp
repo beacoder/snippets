2c3ea9b42c2f56e3111f99df3c68c0679e663415
陈虎明 <chenhuming@gmail.com>
2013-07-12 16:19:51 +0800

    Update algorithms.cpp

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,3 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <assert.h> 
+
+using namespace std;
+
+void BubbleSort(int *number, int n);
+void SelectionSort(int *number, int n);
+
+void PrintArray(int *number, int n)
+{
+  for(int i = 0; i < n; ++i)
+    {
+      cout << number[i] << std::string(" ");
+    }
+
+  cout << std::endl;
+}
+
+int main(void)
+{
+  int numbers[10];
+  for(int i=0; i<10; ++i)
+    {
+      cin >> numbers[i];
+    }
+
+  // BubbleSort(numbers, 10);
+  SelectionSort(numbers, 10);
+
+  PrintArray(numbers, 10);
+
+  return 0;
+}
+
 // 冒泡排序
 void BubbleSort(int *number, int n)
 {
