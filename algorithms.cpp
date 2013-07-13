ab24ac30a08f7d87da3e1dc9147e32a74ac45e30
chm <chenhuming@gmail.com>
2013-07-13 11:30:36 +0800

    some change

--- algorithms.cpp
+++ algorithms.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <assert.h> 
+#include <assert.h>
 
 using namespace std;
 
@@ -9,61 +9,61 @@ void SelectionSort(int *number, int n);
 
 void PrintArray(int *number, int n)
 {
-  for(int i = 0; i < n; ++i)
+    for(int i = 0; i < n; ++i)
     {
-      cout << number[i] << std::string(" ");
+        cout << number[i] << std::string(" ");
     }
 
-  cout << std::endl;
+    cout << std::endl;
 }
 
 int main(void)
 {
-  int numbers[10];
-  for(int i=0; i<10; ++i)
+    int numbers[10];
+    for(int i=0; i<10; ++i)
     {
-      cin >> numbers[i];
+        cin >> numbers[i];
     }
 
-  // BubbleSort(numbers, 10);
-  SelectionSort(numbers, 10);
+    // BubbleSort(numbers, 10);
+    SelectionSort(numbers, 10);
 
-  PrintArray(numbers, 10);
+    PrintArray(numbers, 10);
 
-  return 0;
+    return 0;
 }
 
 // 冒泡排序
 void BubbleSort(int *number, int n)
 {
-  int temp = 0;
-  for(int i = n; i > 0; --i)
-    {      
-      for(int j = 0; j < i-1; ++j)
+    int temp = 0;
+    for(int i = n; i > 0; --i)
     {
-	  if(number[j] > number[j+1])
-	    {
-            std::swap(number[j], number[j+1]);
-	    }
-	}
+        for(int j = 0; j < i-1; ++j)
+        {
+            if(number[j] > number[j+1])
+            {
+                std::swap(number[j], number[j+1]);
+            }
+        }
     }
 }
 
 // 选择排序
 void SelectionSort(int *number, int n)
 {
-  for(int j = 0; j < n; ++j)
+    for(int j = 0; j < n; ++j)
     {
-      int min = j;
+        int min = j;
 
-      for(int i = j; i < n; ++i)
-	{
-	  if(number[min] > number[i])
-	    {
-	      min = i;
-	    }
-	}
+        for(int i = j; i < n; ++i)
+        {
+            if(number[min] > number[i])
+            {
+                min = i;
+            }
+        }
 
-      std::swap(number[j], number[min]);	  
+        std::swap(number[j], number[min]);
     }
 }
