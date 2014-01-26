8beb12b4cde2116f52249e933e8ccdca8fee26dc
bright <bright@bright-ThinkPad-Edge.(none)>
2014-01-27 02:34:24 +0800

    add thread for lua

--- Scripts/sleep4lua.c
+++ Scripts/sleep4lua.c
@@ -2,23 +2,25 @@
  * sleep() function for lua to use
  * gcc -shared -fPIC -o sleep4lua.so -I/usr/local/include -llua sleep4lua.c
  *
-*/
+ */
 
 #include <unistd.h>
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
 
-static int msleep_c(lua_State *L){
-	long msecs = lua_tointeger(L, -1);
-	usleep(1000*msecs);
-	return 0;                  /* No items returned */
+static int msleep_c(lua_State *L)
+{
+  long msecs = lua_tointeger(L, -1);
+  usleep(1000*msecs);
+  return 0;                  /* No items returned */
 }
 
-static int sleep_c(lua_State *L){
-	long secs = lua_tointeger(L, -1);
-	sleep(secs);
-	return 0;                  /* No items returned */
+static int sleep_c(lua_State *L)
+{
+  long secs = lua_tointeger(L, -1);
+  sleep(secs);
+  return 0;                  /* No items returned */
 }
 
 /**
@@ -27,8 +29,9 @@ static int sleep_c(lua_State *L){
  */
 
 /* Register both functions */
-int luaopen_sleep4lua(lua_State *L){
-	lua_register( L, "msleep", msleep_c);  
-	lua_register(L, "sleep", sleep_c);
-	return 0;
+int luaopen_sleep4lua(lua_State *L)
+{
+  lua_register(L, "msleep", msleep_c);  
+  lua_register(L, "sleep", sleep_c);
+  return 0;
 }
---
+++ Scripts/thread4lua.c
@@ -0,0 +1,53 @@
+/*
+ * pthread for lua to use
+ * gcc -shared -fPIC -o thread4lua.so -I/usr/local/include -I/usr/include
+ * -llua -pthread thread4lua.c
+ *
+ */
+
+#include <pthread.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+static int thread_join_c(lua_State *L)
+{
+  /* -1 refers to the element at the stack top
+   *  1 refers to the element at the stack bottom
+   */
+  pthread_t thread = lua_tonumber(L, -1);
+  void* status;
+  if (pthread_join(thread, &status))
+  {
+    lua_pushnumber(L, (int)status);
+  }
+  return 1;                  /* number of results to be returned  */
+}
+
+static int thread_create_c(lua_State *L)
+{
+  /* -1 refers to the element at the stack top
+   *  1 refers to the element at the stack bottom
+   */
+  pthread_t thread;  
+  lua_CFunction proc = lua_tocfunction(L, -1);
+  int arg  = lua_tonumber(L, -2);  
+  if (pthread_create(&thread, NULL, (void *(*) (void *))proc, &arg))
+  {
+    lua_pushnumber(L, thread);
+  }
+  return 1;                  /* number of results to be returned  */
+}
+
+/**
+ * luaopen_thread4lua must contain the shared library name "thread4lua"
+ * so that require("thread4lua") will load thread4lua.so
+ */
+
+/* Register both functions */
+int luaopen_thread4lua(lua_State *L)
+{
+  lua_register(L, "thread_create", thread_create_c);  
+  lua_register(L, "thread_join", thread_join_c);
+  return 0;
+}
