e40f9559438447d73c6fdff93205024d2a105cc3
陈虎明 <chenhuming@gmail.com>
2014-02-13 11:07:23 +0800

    Create umask.sh

---
+++ Scripts/umask.sh
@@ -0,0 +1,22 @@
+#!/bin/ksh
+
+#umask value => file permission
+#0022 => "666-22" => "644" => rw-r--r--
+#0002 => "666-2" => "664" => rw-rw-r--
+
+#global varialbes
+old=0
+new=0
+
+#save old umask
+old=$(umask)
+echo old 'umask' is "$old"
+
+#set new umask
+umask 0002
+new=$(umask)
+echo new 'umask' is "$new"
+
+#restore old umask
+umask $old
+echo current 'umask' is "$(umask)"
