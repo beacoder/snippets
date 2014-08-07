70e8adaa42755f3491eadd8f05a6c6645a644d84
陈虎明 <chenhuming@gmail.com>
2014-08-07 15:19:36 +0800

    Update multi-thread-test.cpp

--- C++/multi-thread-test.cpp
+++ C++/multi-thread-test.cpp
@@ -1,7 +1,7 @@
-/** 
+/**
  * gcc compile command is :
- * g++ -g -Wall -I/usr/include/boost -L/usr/lib -lboost_thread multi-thread-test.cpp -o test 
-*/
+ * g++ -g -Wall -I/usr/include/boost -L/usr/lib -lboost_thread multi-thread-test.cpp -o test
+ */
 
 #include <boost/shared_ptr.hpp>
 #include <iostream>
@@ -11,50 +11,65 @@
 using namespace std;
 
 struct Shared_Obj {
-            
-    Shared_Obj() {
-        std::cout << "Shared_Obj is constructed !" << std::endl;        
-    }
-    
-    ~Shared_Obj() {
-        std::cout << "Shared_Obj is destructed !" << std::endl;        
-    }
+  Shared_Obj() {
+    std::cout << "Shared_Obj is constructed !" << std::endl;
+  }
+
+  ~Shared_Obj() {
+    std::cout << "Shared_Obj is destructed !" << std::endl;
+  }
 };
 
 typedef boost::shared_ptr<Shared_Obj> ResourceType;
 
 void fun1 (ResourceType rs)
 {
-    std::cout << "fun1 is called !" << std::endl;
-    std::cout << "ref_count is  " << rs.use_count() << std::endl;
+  std::cout << "fun1 is called !" << std::endl;
+  std::cout << "ref_count is  " << rs.use_count() << std::endl;
 }
 
 void fun2 (ResourceType rs)
 {
-    std::cout << "fun2 is called !" << std::endl;
-    std::cout << "ref_count is  " << rs.use_count() << std::endl;    
+  std::cout << "fun2 is called !" << std::endl;
+  std::cout << "ref_count is  " << rs.use_count() << std::endl;
 }
+
+struct functor {
+  void operator()(const ResourceType& rs)
+  {
+    std::cout << "functor is called !" << std::endl;
+    std::cout << "ref_count is  " << rs.use_count() << std::endl;
+  }
+};
 
 int main(int argc, char *argv[]) {
 
-    ResourceType rs (new Shared_Obj());
+  ResourceType rs (new Shared_Obj());
 
-    std::cout << "ref_count is  " << rs.use_count() << std::endl;    
+  std::cout << "ref_count is  " << rs.use_count() << std::endl;
 
-    boost::function0<void> threadfunc1 = boost::bind(&fun1, rs);
+  boost::function0<void> threadfunc1 = boost::bind(&fun1, rs);
 
-    std::cout << "ref_count is  " << rs.use_count() << std::endl;
-        
-    boost::function0<void> threadfunc2 = boost::bind(&fun2, rs);
+  std::cout << "ref_count is  " << rs.use_count() << std::endl;
 
-    std::cout << "ref_count is  " << rs.use_count() << std::endl;
-        
-    boost::thread t1(threadfunc1);
-    boost::thread t2(threadfunc2);
-    
-    // join threads, make sure no memory leaks
-    t1.join();
-    t2.join();
-    
-    return 0;
+  boost::function0<void> threadfunc2 = boost::bind(&fun2, rs);
+
+  std::cout << "ref_count is  " << rs.use_count() << std::endl;
+
+  functor fun;
+  boost::function0<void> threadfunc3 =
+      boost::bind(&functor::operator(), &fun, boost::cref(rs));
+
+  std::cout << "ref_count is  " << rs.use_count() << std::endl;
+
+  boost::thread t1(threadfunc1);
+  boost::thread t2(threadfunc2);
+  boost::thread t3(threadfunc3);
+
+  // join threads, make sure no memory leaks
+  t1.join();
+  t2.join();
+  t3.join();
+
+  return 0;
 }
