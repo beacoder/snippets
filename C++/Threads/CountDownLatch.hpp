9d2f99a49bb2c573cd6b7b162cffc9da1a301075
beacoder <chenhuming@gmail.com>
2016-01-11 18:25:34 +0800

    Create CountDownLatch.hpp

---
+++ C++/Threads/CountDownLatch.hpp
@@ -0,0 +1,47 @@
+
+#include <muduo/base/Condition.h>
+#include <muduo/base/Mutex.h>
+
+#include <boost/noncopyable.hpp>
+
+class CountDownLatch : boost::noncopyable
+{
+ public:
+
+  explicit  CountDownLatch(int count)
+            : mutex_(),
+            condition_(mutex_),
+            count_(count)
+  {
+  }
+
+  void wait()
+  {
+    MutexLockGuard lock(mutex_);
+    while (count_ > 0)
+    {
+      condition_.wait();
+    }
+  }
+
+  void countDown()
+  {
+    MutexLockGuard lock(mutex_);
+    --count_;
+    if (count_ == 0)
+    {
+      condition_.notifyAll();
+    }
+  }
+
+  int getCount() const
+  {
+    MutexLockGuard lock(mutex_);
+    return count_;
+  }
+
+ private:
+  mutable MutexLock mutex_;
+  Condition condition_;
+  int count_;
+};
