b85659484493b086c3e69ce8cd3d610d23305f43
beacoder <chenhuming@gmail.com>
2016-01-12 22:29:09 +0800

    Update BlockingQueue.hpp

--- C++/Threads/BlockingQueue.hpp
+++ C++/Threads/BlockingQueue.hpp
@@ -1,13 +1,9 @@
-
-/*
- * @note Modified from chenshuo's "muduo/base/BlockingQueue.h"
- */
-
-#include <mutex>
-#include <condition_variable>
+#include <boost/noncopyable.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition_variable.hpp>
+#include <boost/thread/locks.hpp>
 #include <deque>
 #include <assert.h>
-#include <boost/noncopyable.hpp>
 
 template<typename T>
 class BlockingQueue : boost::noncopyable
@@ -15,7 +11,7 @@ class BlockingQueue : boost::noncopyable
  public:
   void put(const T& x)
   {
-    std::lock_guard lock(mutex_);
+    boost::lock_guard lock(mutex_);
     queue_.push_back(x);
     condition_.notify(); // wait morphing saves us
     // http://www.domaigne.com/blog/computing/condvars-signal-with-mutex-locked-or-not/
@@ -23,7 +19,7 @@ class BlockingQueue : boost::noncopyable
 
   T take()
   {
-    std::lock_guard lock(mutex_);
+    boost::lock_guard lock(mutex_);
     // always use a while-loop, due to spurious wakeup
     while (queue_.empty())
     {
@@ -37,18 +33,18 @@ class BlockingQueue : boost::noncopyable
 
   size_t size() const
   {
-    std::lock_guard lock(mutex_);
+    boost::lock_guard lock(mutex_);
     return queue_.size();
   }
   
   bool empty() const
   {
-    std::lock_guard lock(mutex_);
+    boost::lock_guard lock(mutex_);
     return queue_.empty();
   }
 
  private:
-  mutable std::mutex      mutex_;
-  std::condition_variable condition_;
-  std::deque<T>           queue_;
+  mutable boost::mutex      mutex_;
+  boost::condition_variable condition_;
+  std::deque<T>             queue_;
 };
