73ea754de172acd8506c4a8283b9659209f9f694
陈虎明 <chenhuming@gmail.com>
2017-05-19 00:01:41 +0800

    Update test.py

--- Scripts/test.py
+++ Scripts/test.py
@@ -9,16 +9,6 @@ import requests
 # Pattern for matching douban movies
 _DOUBAN_MOVIES_PATTERN = re.compile(r'href="(https?://\S+/subject/\d+/)\S*"')
 
-# Pattern for matching rating numble against douban movie
-_DOUBAN_RATING_NUMBER_PATTERN = re.compile(
-    r'<strong class="ll rating_num" property="v:average">(.*)</strong>')
-
-# Pattern for matching douban movie name
-_DOUBAN_MOVIE_NAME_PATTERN = re.compile(r'<span property="v:itemreviewed">(.*)</span>')
-
-# Pattern for matching year of douban movie
-_DOUBAN_MOVIE_YEAR_PATTERN = re.compile(r'<span class="year">(.*)</span>')
-
 def str_to_utf8(in_string):
     "Convert string to utf-8 encoding."
     ret_string = u' '.join(in_string).encode('utf-8').strip()
@@ -28,54 +18,53 @@ def remove_duplicates_from_list(seq):
     "Remove duplicate items from a list."
     seq = list(set(seq))
     return seq
-    
-def get_rating_num(url):
-    "Get the rating number of a movie."
-    try:
-        return float((re.findall(_DOUBAN_RATING_NUMBER_PATTERN,
-                                 requests.get(url).text))[0])
-    except BaseException, e:
-        print e
-        return None
 
-    def get_movie_name(url):
-    "Get the movie name."
-    try:
-        return float((re.findall(_DOUBAN_MOVIE_NAME_PATTERN,
-                                 requests.get(url).text))[0])
-    except BaseException, e:
-        print e
-        return None
 
-def get_movie_year(url):
-    "Get year of a movie."
-    
 class DoubanMovie(object):
     """Represent a movie from movie.douban.com"""
 
-    def __init__(self,
-                 url):
-        self.url = url
+    # Pattern for matching rating numble against douban movie
+    _DOUBAN_RATING_NUMBER_PATTERN = re.compile(
+        r'<strong class="ll rating_num" property="v:average">(.*)</strong>')
+
+    # Pattern for matching douban movie name
+    _DOUBAN_MOVIE_NAME_PATTERN    = re.compile(r'<span property="v:itemreviewed">(.*)</span>')
+
+    # Pattern for matching year of douban movie
+    _DOUBAN_MOVIE_YEAR_PATTERN    = re.compile(r'<span class="year">(.*)</span>')
+
+    def __init__(self, url):
+        self._rating_num = ""
+        self._movie_name = ""
+        self._movie_year = ""
+        self._parse_movie(url)
+
+    def _parse_movie(self, url):
+        rsp = requests.get(url).text
+        try:
+            self._rating_num = (re.findall(DoubanMovie._DOUBAN_RATING_NUMBER_PATTERN, rsp))[0]
+            self._movie_name = (re.findall(DoubanMovie._DOUBAN_MOVIE_NAME_PATTERN, rsp))[0]
+            self._movie_year = (re.findall(DoubanMovie._DOUBAN_MOVIE_YEAR_PATTERN, rsp))[0]
+        except BaseException, e:
+            print e
+
+    def __repr__(self):
+        return ("Rate of %s%s is %s \n" % (self._movie_name, self._movie_year, self._rating_num))
 
     @property
     def rating_num(self):
         "Get the rating number of a movie."
-        try:
-            return float((re.findall(_DOUBAN_RATING_NUMBER_PATTERN,
-                                     requests.get(url).text))[0])
-        except BaseException, e:
-            print e
-            return None
+        return self._rating_num
 
-    def get_movie_name(url):
+    @property
+    def movie_name(self):
         "Get the movie name."
-        try:
-            return float((re.findall(_DOUBAN_MOVIE_NAME_PATTERN,
-                                     requests.get(url).text))[0])
-        except BaseException, e:
-            print e
-            return None
+        return self._movie_name
 
+    @property
+    def movie_year(self):
+        "Get the movie year."
+        return self._movie_year
 
 def test():
     """Start crawling."""
@@ -84,10 +73,8 @@ def test():
     urls = remove_duplicates_from_list(re.findall(_DOUBAN_MOVIES_PATTERN, rsp.text))
 
     for l in urls:
-        r = get_rating_num(l)
-        if r is None:
-            r = 0.00
-        print("Rate of %s is %f \n" % (l, r))
+        movie = DoubanMovie(l)
+        print(movie.__repr__())
 
 if __name__ == '__main__' :
-    test()    
+    test()
