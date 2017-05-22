6ea3f1004d4882f0ec9f061a99d3c6640ec79b51
陈虎明 <chenhuming@gmail.com>
2017-05-22 23:50:46 +0800

    Update douban_crawler.py

--- Scripts/douban_crawler.py
+++ Scripts/douban_crawler.py
@@ -4,60 +4,104 @@
 
 import re
 import requests
+from collections import deque
 
 
 # Pattern for matching douban movies
-_DOUBAN_MOVIES_PATTERN = re.compile(
-    r'href="(https?://\S+/subject/\d+/)\S*"')
+_DOUBAN_MOVIES_PATTERN = re.compile(r'href="(https?://\S+/subject/\d+/)\S*"')
+
+_DOUBAN_MOVIE_SEED     = "https://movie.douban.com"
+
+_CRAWLED_SITES         = set()
 
 def str_to_utf8(in_string):
     "Convert string to utf-8 encoding."
     ret_string = u' '.join(in_string).encode('utf-8').strip()
     return ret_string
 
-def remove_duplicates(seq):
-    "Remove duplicate items from a list."
-    seq = list(set(seq))
-    return seq
+class _Base_For_Queue_And_Stack:
+    """Base class for Queue and Stack."""
+    def __init__(self):
+        self.items = deque()
+
+    def isEmpty(self):
+        return (len(self.items) == 0)
+
+    def __iter__(self):
+        return self
+
+    def size(self):
+        return len(self.items)
+
+
+class Queue(_Base_For_Queue_And_Stack):
+    """FIFO Container."""
+    def next(self):
+        try:
+            return self.deque()
+        except IndexError:
+            raise StopIteration
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def deque(self):
+        return self.items.popleft()
+
+
+class Stack(_Base_For_Queue_And_Stack):
+    """LIFO Container."""
+    def next(self):
+        try:
+            return self.pop()
+        except IndexError:
+            raise StopIteration
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
 
 
 class DoubanMovie(object):
     """Represent a movie from movie.douban.com"""
 
     # Pattern for matching rating numble against douban movie
-    _DOUBAN_RATING_NUMBER_PATTERN = re.compile(
-        r'<strong class="ll rating_num" property="v:average">(.*)</strong>')
+    _DOUBAN_MOVIE_RATE_PATTERN = re.compile(r'<strong class="ll rating_num" property="v:average">(.*)</strong>')
 
     # Pattern for matching douban movie name
-    _DOUBAN_MOVIE_NAME_PATTERN    = re.compile(
-        r'<span property="v:itemreviewed">(.*)</span>')
+    _DOUBAN_MOVIE_NAME_PATTERN    = re.compile(r'<span property="v:itemreviewed">(.*)</span>')
 
     # Pattern for matching year of douban movie
-    _DOUBAN_MOVIE_YEAR_PATTERN    = re.compile(
-        r'<span class="year">(.*)</span>')
+    _DOUBAN_MOVIE_YEAR_PATTERN    = re.compile(r'<span class="year">(.*)</span>')
 
     def __init__(self, url):
-        self._rating_num = ""
+        self._movie_rate = ""
         self._movie_name = ""
         self._movie_year = ""
         self._parse_movie(url)
 
     def _parse_movie(self, url):
-        rsp = requests.get(url).text
-        try:
-            self._rating_num = (re.findall(DoubanMovie._DOUBAN_RATING_NUMBER_PATTERN, rsp))[0]
-            self._movie_name = (re.findall(DoubanMovie._DOUBAN_MOVIE_NAME_PATTERN, rsp))[0]
-            self._movie_year = (re.findall(DoubanMovie._DOUBAN_MOVIE_YEAR_PATTERN, rsp))[0]
-        except BaseException, e:
-            print e
+        with requests.Session() as s:
+            rsp = s.get(url).text
+            try:
+                self._movie_rate = (re.findall(DoubanMovie._DOUBAN_MOVIE_RATE_PATTERN, rsp))[0]
+                self._movie_name = (re.findall(DoubanMovie._DOUBAN_MOVIE_NAME_PATTERN, rsp))[0]
+                self._movie_year = (re.findall(DoubanMovie._DOUBAN_MOVIE_YEAR_PATTERN, rsp))[0]
+            except BaseException, e:
+                print e
 
     def __repr__(self):
-        return ("Rate of %s%s is %s \n" % (self._movie_name, self._movie_year, self._rating_num))
+        return ("Rate of %s%s is %s \n" % (self._movie_name, self._movie_year, self._movie_rate))
 
     @property
-    def rating_num(self):
-        "Get the rating number of a movie."
-        return self._rating_num
+    def movie_rate(self):
+        "Get the movie rate."
+        return self._movie_rate
 
     @property
     def movie_name(self):
@@ -69,15 +113,23 @@ class DoubanMovie(object):
         "Get the movie year."
         return self._movie_year
 
-def crawl():
-    """Start crawling."""
-    # Crawl douban movie
-    rsp  = requests.get("https://movie.douban.com")
-    urls = remove_duplicates(re.findall(_DOUBAN_MOVIES_PATTERN, rsp.text))
 
-    for movie_site in urls:
-        movie = DoubanMovie(movie_site)
-        print(movie.__repr__())
+# Normally, BFS will be a good idea for web crawling.
+# @see http://stackoverflow.com/questions/20579169/dfs-vs-bfs-in-web-crawler-design
+def bfs_crawl(seed):
+    """Breadth First Crawling."""
+
+    q = Queue()
+    q.enqueue(seed)
+
+    with requests.Session() as s:
+        while not q.isEmpty():
+            urls = set(re.findall(_DOUBAN_MOVIES_PATTERN, s.get(q.deque()).text))
+            for url in urls:
+                q.enqueue(url)
+                movie = DoubanMovie(url)
+                print(movie.__repr__())
+
 
 if __name__ == '__main__' :
-    crawl()
+    bfs_crawl(_DOUBAN_MOVIE_SEED)
