119886bdebbd5991e3e4c88d258e3ab6de1fe6bd
陈虎明 <chenhuming@gmail.com>
2017-08-11 00:30:20 +0800

    Update C++11.cpp

--- C++/Misc/C++11.cpp
+++ C++/Misc/C++11.cpp
@@ -29,3 +29,18 @@ int main()
     foo f;
     f.invoke(1, 2);
 }
+
+
+// std::bind() vs lambdas in C++14
+
+// move and capture varaibles
+auto f1 = std::bind(f, 42, _1, std::move(v));
+auto f1 = [v = std::move(v)](auto arg) { f(42, arg, std::move(v)); };
+
+// capture expressions
+auto f1 = std::bind(f, 42, _1, a + b);
+auto f1 = [sum = a + b](auto arg) { f(42, arg, sum); };
+
+// perfect forwarding
+auto f1 = std::bind(f, 42, std::forward<Args>(args)...);
+auto f1 = [=](auto&& arg) { f(42, std::forward<decltype(arg)>(arg)); };
