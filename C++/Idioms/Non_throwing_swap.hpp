7bf6673a114df1b04b15f5a17d0d1bab83edc85d
陈虎明 <chenhuming@gmail.com>
2017-08-10 18:59:45 +0800

    Update Non_throwing_swap.hpp

--- C++/Idioms/Non_throwing_swap.hpp
+++ C++/Idioms/Non_throwing_swap.hpp
@@ -1,18 +1,20 @@
 class UserDefined 
 {
-    String str;
   public:
-    void swap (UserDefined & u) noexcept
+    void swap (UserDefined& u) noexcept
     { 
       std::swap (str, u.str); 
     }
+
+  private:
+    String str;
 };
 
 namespace std
 {
   // Full specializations of the templates in std namespace can be added in std namespace.
   template <>
-  void swap (UserDefined & u1, UserDefined & u2) noexcept
+  void swap (UserDefined& u1, UserDefined& u2) noexcept
   {
     u1.swap (u2);
   }
@@ -20,21 +22,23 @@ namespace std
 
 class Myclass
 {
-    UserDefined u;
-    char * name;
   public:
-    void swap (Myclass & m) noexcept
+    void swap (Myclass& m) noexcept
     {
       std::swap (u, m.u);       // cascading use of the idiom due to uniformity
       std::swap (name, m.name); // Ditto here
-    }   
+    }
+
+  private:
+    UserDefined u;
+    char * name;
 }
 
 namespace std
 {
    // Full specializations of the templates in std namespace can be added in std namespace.
    template <> 
-   void swap (Myclass & m1, Myclass & m2) noexcept
+   void swap (Myclass& m1, Myclass& m2) noexcept
    {
      m1.swap (m2);
    }
