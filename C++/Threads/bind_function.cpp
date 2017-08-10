ca2cd66955dee5bdd01c1106f184138ae7e8d8f8
陈虎明 <chenhuming@gmail.com>
2017-08-10 18:39:51 +0800

    Update bind_function.cpp

--- C++/Threads/bind_function.cpp
+++ C++/Threads/bind_function.cpp
@@ -1,12 +1,12 @@
 /**
  * gcc compile command is :
- * g++ -g -Wall -I/usr/include/boost -L/usr/lib -lboost_thread bind_function.cpp -o test
+ * g++ -g -Wall -L/usr/lib bind_function.cpp -o test
  */
 
 #include <memory>
+#include <functional>
 #include <iostream>
-#include <boost/bind.hpp>
-#include <boost/thread.hpp>
+#include <thread.hpp>
 
 using namespace std;
 
@@ -47,26 +47,26 @@ struct functor {
 
 int main(int argc, char *argv[]) {
 
-  ResourceType rs = boost::make_shared<Shared_Obj>();
+  ResourceType rs = std::make_shared<Shared_Obj>();
 
   std::cout << "ref_count is  " << rs.use_count() << std::endl;
 
-  boost::function0<void> threadfunc1 = boost::bind(&fun1, rs);
+  std::function<void> threadfunc1 = std::bind(&fun1, rs);
 
   std::cout << "ref_count is  " << rs.use_count() << std::endl;
 
-  boost::function0<void> threadfunc2 = boost::bind(&fun2, rs);
+  std::function<void> threadfunc2 = std::bind(&fun2, rs);
 
   std::cout << "ref_count is  " << rs.use_count() << std::endl;
 
   functor fun;
-  boost::function0<void> threadfunc3 = boost::bind(&functor::operator(), &fun, boost::cref(rs));
+  std::function<void> threadfunc3 = std::bind(&functor::operator(), &fun, std::cref(rs));
 
   std::cout << "ref_count is  " << rs.use_count() << std::endl;
 
-  boost::thread t1(threadfunc1);
-  boost::thread t2(threadfunc2);
-  boost::thread t3(threadfunc3);
+  std::thread t1(threadfunc1);
+  std::thread t2(threadfunc2);
+  std::thread t3(threadfunc3);
 
   // join threads, make sure no memory leaks
   t1.join();
