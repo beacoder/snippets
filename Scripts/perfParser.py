67f66c2bb67955e385261b4b8426d0bcf386b119
bright <bright@gmail.com>
2017-09-05 01:13:36 +0800

    Update perfParser.py

--- Scripts/perfParser.py
+++ Scripts/perfParser.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 """This script is used for parsing perf results."""
 
+from collections import defaultdict
 import re
 import argparse
 import json
 import sys
+import pdb
+
+nested_dict = lambda: defaultdict(nested_dict)
+
+# nd = nested_dict()
+# nd["a","b","c"] = 'local'
+# assert nd["a"]["b"]["c"] == 'local'
 
 def usage():
+    """usage information"""
+
     sample ="""Parse perf output file and generate funciton calling statistics:
 e.g: ./perfParser.py -f out.txt -o statistics
-  """
+    """
     print sample
 
-def doParseLine(line):
-    pass
 
+def doParseLine(line):
+    (call_chain, times) = line.split(" ")
+    print call_chain
+    print times
+
+    d = {}
+    for key in reversed(call_chain.split(";")):
+        d = {key: d}
 
 
 def doParseFile(input_file, output_file):
@@ -24,12 +40,12 @@ def doParseFile(input_file, output_file):
         doParse(line)
 
   # Filter python objects with list comprehensions
-  output_dict = [x for x in input_dict if doMatch(x, filters)]
+  # output_dict = [x for x in input_dict if doMatch(x, filters)]
 
   # Transform python object back into json
-  with open(output_json, 'w') as outfile:
-      json.dump(output_dict, outfile, indent=2)
-  pass
+  # with open(output_json, 'w') as outfile:
+  #     json.dump(output_dict, outfile, indent=2)
+  # pass
 
 
 def createParser():
@@ -38,6 +54,7 @@ def createParser():
     p.add_argument("-o")
 
     return p.parse_args()
+
 
 def main():
     args = createParser()
