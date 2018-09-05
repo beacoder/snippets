7b27d0b2b6a62add63a3dfa515c414e20acebca1
陈虎明 <chenhuming@gmail.com>
2018-09-06 00:24:21 +0800

    Update server.py

--- Scripts/Chatting/server.py
+++ Scripts/Chatting/server.py
@@ -15,4 +15,57 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import os
+import time
 from socketserver import BaseRequestHandler, UDPServer
+
+
+def check_db():
+    """Check if db is ready.
+
+    Check if db is ready, if not, create db and initialize
+    all tables.
+    """
+
+    if not os.path.exists('./db.sqlite'):
+        from sqlalchemy import create_engine
+        from sqlalchemy import MetaData
+        from sqlalchemy import Table
+        from sqlalchemy import Column
+        from sqlalchemy import Integer, String
+
+        db_uri   = 'sqlite:///db.sqlite'
+        engine   = create_engine(db_uri)
+        metadata = MetaData(engine)
+
+        users_table = Table('Users', metadata,
+                            Column('id', Integer, primary_key=True),
+                            Column('ipaddr', String),
+                            Column('message', String))
+        metadata.create_all()
+
+        for _t in metadata.tables:
+           print("Table: ", _t)
+    pass
+
+
+class EchoHandler(BaseRequestHandler):
+    def handle(self):
+        data, sock = self.request
+        print('Got message {msg} from {peer}.'.format(msg=data,
+              peer=self.client_address))
+        print('Sent message {msg} to {peer}.'.format(msg=data,
+              peer=self.client_address))
+        print('')
+        sock.sendto(data, self.client_address)
+
+
+def main():
+    check_db()
+
+    with UDPServer(('', 1223), EchoHandler) as serv:
+        serv.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
