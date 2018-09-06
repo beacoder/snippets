ec95c5c2c554ff7d0bc923e762f28386a1db6e59
陈虎明 <chenhuming@gmail.com>
2018-09-06 12:44:33 +0800

    Create server.py

---
+++ Scripts/Chatting/v2.6/server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018-2018 humingchen
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import socket
+
+class UDPServer(object):
+    def __init__(self,host,port):
+        self._host = host
+        self._port = port
+
+    def __enter__(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((self._host,self._port))
+        self._sock = sock
+        return sock
+
+    def __exit__(self,*exc_info):
+        if exc_info[0]:
+            import traceback
+            traceback.print_exception(*exc_info)
+        self._sock.close()
+
+if __name__ == '__main__':
+    host = 'localhost'
+    port = 5566
+    with UDPServer(host,port) as s:
+        while True:
+            msg, addr = s.recvfrom(1024)
+            s.sendto(msg, addr)
