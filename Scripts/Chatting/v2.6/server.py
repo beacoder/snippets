db3890a5c49fb9723bf9ad22713ddff086aacb22
陈虎明 <chenhuming@gmail.com>
2018-09-15 01:39:03 +0800

    Update server.py

--- Scripts/Chatting/v2.6/server.py
+++ Scripts/Chatting/v2.6/server.py
@@ -15,6 +15,9 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from __future__ import absolute_import, division, print_function, \
+    with_statement
+
 import socket
 
 
